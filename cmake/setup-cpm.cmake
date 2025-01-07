cbe23b86caa5c9676f38bcf1e6ca0d1aa651385a
alin <alin@rspsystems.com>
2025-01-07 16:10:15 +0100

    Add setup CPM script (incomplete)

---
+++ cmake/setup-cpm.cmake
@@ -0,0 +1,6 @@
+# TODO: This script is intended to CPM package manager
+# TODO: @see https://github.com/cpm-cmake/CPM.cmake
+# TODO: @see https://github.com/adobe/lagrange/blob/main/cmake/recipes/external/CPM.cmake
+
+# TODO: Perhaps it should be designed as a function to be invoked, e.g.
+# TODO: setupCPM(VERSION 0.40.5)
