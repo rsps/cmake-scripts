6658618dff6dfd5b4b6710823f28af6ea5ae725a
alin <alin@rspsystems.com>
2025-01-09 10:07:49 +0100

    Add dependencies scripts
    Intended to contain all of this package's dependencies and development dependencies

---
+++ dev-dependencies.cmake
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------------------------------------------- #
+# Development Dependencies
+# -------------------------------------------------------------------------------------------------------------- #
+
+include_guard()
+
+# Include regular dependencies
+include("dependencies.cmake")
+
+message(STATUS "Installing Development Dependencies for ${PROJECT_NAME}")
+
+# TODO: Add dependencies here...
