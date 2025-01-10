3d49f2556546037944f97c23eec10a85d7526811
alin <alin@rspsystems.com>
2025-01-10 15:51:21 +0100

    Add internal remarks / TODO list

--- cmake/rsp/git.cmake
+++ cmake/rsp/git.cmake
@@ -8,3 +8,13 @@ include_guard(GLOBAL)
 message(VERBOSE "rsp/git module included")
 
 # TODO: incomplete...
+
+# TODO: To obtain local git tags, sorted acc. to their version:
+# TODO:     git tag --sort=-version:refname
+# TODO:     git tag --list --sort=-version:refname
+# TODO: @see https://blog.boot.dev/open-source/view-git-tags-with-semver-ordering/
+
+# TODO: Inspiration for setting in a file, via cmake...
+# TODO: @see https://www.marcusfolkesson.se/blog/git-version-in-cmake/
+# TODO: @see https://gitlab.orfeo-toolbox.org/bradh/otb/-/blob/develop/CMake/GetVersionFromGitTag.cmake
+# TODO: @see https://github.com/nocnokneo/cmake-git-versioning-example/blob/master/GenerateVersionHeader.cmake
