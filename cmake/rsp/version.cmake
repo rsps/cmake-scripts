882ac786d111c8d50dc206646e2d6556154ea55d
alin <alin@rspsystems.com>
2025-01-12 15:59:59 +0100

    Add version_from_file() util
    Function attempts to read semantic version from specified file.

--- cmake/rsp/version.cmake
+++ cmake/rsp/version.cmake
@@ -48,7 +48,6 @@ if (NOT COMMAND "semver_parse")
         endforeach ()
 
         # Remove eventual "v" prefix from given version string
-        set(cleanVersion "${INPUT_VERSION}")
         string(REGEX REPLACE "^[v]" "" cleanVersion "${INPUT_VERSION}")
 
         # Parse given version string
@@ -90,6 +89,7 @@ if (NOT COMMAND "write_version_file")
     #! write_version_file : Writes a version string to given file
     #
     # @see rsp/git::git_find_version_tag()
+    # @see rsp/version::version_from_file()
     #
     # @example
     #       # Defaults to version obtained via git
@@ -148,6 +148,7 @@ if (NOT COMMAND "write_version_file")
             # version strings ("[..] A 255 character version string is probably overkill [...]").
             # @see https://semver.org/#does-semver-have-a-size-limit-on-the-version-string
             file(READ ${INPUT_FILE} existing LENGTH_MAXIMUM 255)
+            string(STRIP "${existing}" existing)
 
             # Avoid changing the version file's modification datetime, if versions match.
             if (existing STREQUAL INPUT_VERSION)
@@ -158,5 +159,120 @@ if (NOT COMMAND "write_version_file")
 
         # Finally, write the version in specified file
         file(WRITE ${INPUT_FILE} ${INPUT_VERSION})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "version_from_file")
+
+    #! version_from_file : Read a semantic version from given file
+    #
+    # @see rsp/version::write_version_file()
+    # @see rsp/version::semver_parse()
+    #
+    # @param FILE <path>            Path to the target file
+    # @param OUTPUT <variable>      The output variable to assign version to
+    # @param [DEFAULT <string>]     Optional default version string to return, when version file
+    #                               does not exist. Defaults to "0.0.0"
+    # @param [EXIT_ON_FAILURE]      OPTION: Throws fatal error if version file does not exist,
+    #                               regardless of the [DEFAULT] argument.
+    #
+    # @return
+    #     [OUTPUT]                 The full version string, e.g. "v3.4.22-beta.3+AF1004"
+    #     [...]                    See semver_parse() for full list of variables
+    #
+    # @throws If [EXIT_ON_FAILURE] option is specified and FILE does not exist, or is empty.
+    # @throws If obtained version is not a valid semantic version (regardless of [EXIT_ON_FAILURE] option)
+    #
+    function(version_from_file)
+        set(options EXIT_ON_FAILURE) # N/A
+        set(oneValueArgs FILE OUTPUT DEFAULT)
+        set(multiValueArgs "") # N/A
+
+        cmake_parse_arguments(INPUT "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+        # Ensure required arguments are defined
+        set(requiredArgs "FILE;OUTPUT")
+        foreach (name ${requiredArgs})
+            if (NOT DEFINED INPUT_${name})
+                message(FATAL_ERROR "${name} argument is missing, for ${CMAKE_CURRENT_FUNCTION}()")
+            endif ()
+        endforeach ()
+
+        # Resolve optional arguments
+        if (NOT DEFINED INPUT_DEFAULT)
+            set(INPUT_DEFAULT "0.0.0")
+        endif ()
+
+        # The version string to be populated and parsed
+        set(version "${INPUT_DEFAULT}")
+
+        # If version file does not exist
+        if (NOT EXISTS ${INPUT_FILE})
+            # Abort if requested to exit on failure
+            if (INPUT_EXIT_ON_FAILURE)
+                message(FATAL_ERROR "Version file ${INPUT_FILE} does not exist")
+            endif ()
+
+            # Debug
+            message(VERBOSE
+                "${CMAKE_CURRENT_FUNCTION}():\n"
+                "Version file ${INPUT_FILE} does not exist\n"
+                "Defaulting to use version: ${version}\n"
+            )
+        endif ()
+
+        # Read the contents of the file (max 255 bytes).
+        # @see https://semver.org/#does-semver-have-a-size-limit-on-the-version-string
+        file(READ ${INPUT_FILE} version LENGTH_MAXIMUM 255)
+        string(STRIP "${version}" version)
+
+        # If version file is empty
+        if (version STREQUAL "")
+            # Abort if requested to exit on failure
+            if (INPUT_EXIT_ON_FAILURE)
+                message(FATAL_ERROR "Version file ${INPUT_FILE} is empty")
+            endif ()
+
+            # Debug
+            message(VERBOSE
+                "${CMAKE_CURRENT_FUNCTION}():\n"
+                "Version file ${INPUT_FILE} is empty\n"
+                "Defaulting to use version: ${INPUT_DEFAULT}\n"
+            )
+
+            # Restore / use default version
+            set(version "${INPUT_DEFAULT}")
+        endif ()
+
+        # Parse obtained version string
+        semver_parse(
+            VERSION "${version}"
+            OUTPUT ${INPUT_OUTPUT}
+        )
+
+        # Debug
+        #        dump(
+        #            "${INPUT_OUTPUT}"
+        #            "${INPUT_OUTPUT}_VERSION"
+        #            "${INPUT_OUTPUT}_SEMVER"
+        #            "${INPUT_OUTPUT}_MAJOR"
+        #            "${INPUT_OUTPUT}_MINOR"
+        #            "${INPUT_OUTPUT}_PATCH"
+        #            "${INPUT_OUTPUT}_PRE_RELEASE"
+        #            "${INPUT_OUTPUT}_BUILD_METADATA"
+        #        )
+
+        # Finally, propagate resulting variables from semver_parse()
+        return(
+            PROPAGATE
+            "${INPUT_OUTPUT}"
+            "${INPUT_OUTPUT}_VERSION"
+            "${INPUT_OUTPUT}_SEMVER"
+            "${INPUT_OUTPUT}_MAJOR"
+            "${INPUT_OUTPUT}_MINOR"
+            "${INPUT_OUTPUT}_PATCH"
+            "${INPUT_OUTPUT}_PRE_RELEASE"
+            "${INPUT_OUTPUT}_BUILD_METADATA"
+        )
     endfunction()
 endif ()
