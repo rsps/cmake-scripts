4da3c6864e588c948bad3c22a2ffd1f6e64a22c1
alin <alin@rspsystems.com>
2025-01-18 17:30:29 +0100

    Prevent test executor from being included into other cmake files

--- cmake/rsp/testing/test_executor.cmake
+++ cmake/rsp/testing/test_executor.cmake
@@ -4,10 +4,21 @@
 #
 # Responsible for including a requested test file (*.cmake) and invoke the requested cmake function.
 #
+# WARNING: Do NOT include this script directly into your project's cmake files. Your build will fail...!
+#
+# -------------------------------------------------------------------------------------------------------------- #
 
-# TODO: Can this file REJECT running, if not within CTest execution process???
+# Fail if executor is attempted included inside a project's CMakeLists.txt, or other cmake file
+if(DEFINED CMAKE_PROJECT_NAME OR DEFINED PROJECT_NAME OR DEFINED PROJECT_IS_TOP_LEVEL)
+    message(FATAL_ERROR
+        "Test Executor is NOT allowed to be included in any cmake script!\n"
+        "The executor is intended to be called as an external process that runs requested test (function), in the specified *.cmake test-case file. "
+        "Please see official documentation for additional information. "
+        "https://github.com/rsps/cmake-scripts"
+    )
+endif ()
 
-# ---------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
 
 # Abort if required variables are not defined
 set(required "TEST_NAME;TEST_CALLBACK;TEST_FILE;MODULE_PATHS")
@@ -17,8 +28,7 @@ foreach (arg ${required})
     endif ()
 endforeach ()
 
-# ---------------------------------------------------------------------------------------------------------- #
-
+# -------------------------------------------------------------------------------------------------------------- #
 # Output a bit of information about the test that is about to be attempted executed.
 # This output will be displayed when adding `--verbose` option to ctest, e.g.
 #       ctest --verbose --test-dir build/tests
@@ -34,7 +44,7 @@ message(NOTICE "File: ${TEST_FILE}")
 message(NOTICE "Module path(s): ${MODULE_PATHS}")
 message(NOTICE "\n")
 
-# ---------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
 # Append provided paths to cmake's module path
 foreach (path ${MODULE_PATHS})
     list(APPEND CMAKE_MODULE_PATH "${path}")
@@ -42,7 +52,7 @@ endforeach ()
 
 list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
 
-# ---------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
 
 # Abort if target test file does not exist
 if (NOT EXISTS ${TEST_FILE})
@@ -55,7 +65,7 @@ set(IGNORE_DEFINE_TEST TRUE)
 # Include the test file
 include(${TEST_FILE})
 
-# ---------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
 
 # Fail if command does not exist...
 if (NOT COMMAND "${TEST_CALLBACK}")
