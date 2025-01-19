baec7f318e543c38dc6d81d413d6f918e9d43eb6
alin <alin@rspsystems.com>
2025-01-19 11:07:10 +0100

    Add test for asserts

---
+++ tests/unit/testing/asserts_test.cmake
@@ -0,0 +1,93 @@
+include("rsp/testing")
+
+# -------------------------------------------------------------------------------------------------------------- #
+# Tests for "Truthy / Falsy" assertions
+# -------------------------------------------------------------------------------------------------------------- #
+
+define_test("can assert truthy keys" "asserts_truthy_keys")
+function(asserts_truthy_keys)
+    set(a TRUE)
+    assert_truthy(a MESSAGE "const 'TRUE'")
+
+    set(b 1)
+    assert_truthy(b MESSAGE "number")
+
+    set(c 3.2)
+    assert_truthy(c MESSAGE "float")
+
+    set(d ON)
+    assert_truthy(d MESSAGE "const 'ON'")
+
+    set(e YES)
+    assert_truthy(e MESSAGE "const 'YES'")
+
+    set(f Y)
+    assert_truthy(f MESSAGE "const 'Y'")
+endfunction()
+
+define_test("can assert truthy values" "asserts_truthy_values")
+function(asserts_truthy_values)
+    assert_truthy(TRUE MESSAGE "const 'TRUE'")
+
+    assert_truthy(1 MESSAGE "number")
+
+    assert_truthy(3.2 MESSAGE "float")
+
+    assert_truthy(ON MESSAGE "const 'ON'")
+
+    assert_truthy(YES MESSAGE "const 'YES'")
+
+    assert_truthy(Y MESSAGE "const 'Y'")
+endfunction()
+
+
+define_test("can assert falsy keys" "asserts_falsy_keys")
+function(asserts_falsy_keys)
+    set(a FALSE)
+    assert_falsy(a MESSAGE "const 'TRUE'")
+
+    set(b 0)
+    assert_falsy(b MESSAGE "number")
+
+    set(c OFF)
+    assert_falsy(c MESSAGE "const 'OFF'")
+
+    set(d NO)
+    assert_falsy(d MESSAGE "const 'NO'")
+
+    set(e N)
+    assert_falsy(e MESSAGE "const 'N'")
+
+    set(f IGNORE)
+    assert_falsy(f MESSAGE "const 'IGNORE'")
+
+    set(g NOTFOUND)
+    assert_falsy(g MESSAGE "const 'NOTFOUND'")
+
+    set(h "")
+    assert_falsy(h MESSAGE "empty string")
+
+    set(i "abc-NOTFOUND")
+    assert_falsy(i MESSAGE "string that end with '-NOTFOUND'")
+endfunction()
+
+define_test("can assert falsy values" "asserts_falsy_values")
+function(asserts_falsy_values)
+    assert_falsy(FALSE MESSAGE "const 'TRUE'")
+
+    assert_falsy(0 MESSAGE "number")
+
+    assert_falsy(OFF MESSAGE "const 'OFF'")
+
+    assert_falsy(NO MESSAGE "const 'NO'")
+
+    assert_falsy(N MESSAGE "const 'N'")
+
+    assert_falsy(IGNORE MESSAGE "const 'IGNORE'")
+
+    assert_falsy(NOTFOUND MESSAGE "const 'NOTFOUND'")
+
+    assert_falsy("" MESSAGE "empty string")
+
+    assert_falsy("abc-NOTFOUND" MESSAGE "string that end with '-NOTFOUND'")
+endfunction()
