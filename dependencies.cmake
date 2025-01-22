23a5b9972733532a7540bdfaee97d4dca0da914a
alin <alin@rspsystems.com>
2025-01-22 10:40:14 +0100

    Wrap installs in callbacks, safeguard the BUILD_TESTING variable

--- dependencies.cmake
+++ dependencies.cmake
@@ -4,7 +4,14 @@
 
 include_guard()
 
-message(VERBOSE "Installing Dependencies for ${PROJECT_NAME}")
+function(install_dependencies)
+    message(STATUS "Installing Dependencies for ${PROJECT_NAME}")
 
-# TODO: Add dependencies here...
-message(VERBOSE "    N/A") # TODO: Cleanup
+    # Avoid building tests for dependencies...
+    set(BUILD_TESTING off)
+
+    # Add dependencies here...
+    message(STATUS "    N/A")
+
+endfunction()
+safeguard_properties(CALLBACK "install_dependencies" PROPERTIES BUILD_TESTING)
