5442c59bbf0a00972cb991b07ddc0266df5ee520
alin <alin@rspsystems.com>
2025-01-28 15:22:23 +0100

    Refactor helpers module, extract dump() and dd() into own module
    We should expect that more debugging utils will be added in future versions and it is therefore a good idea to have a custom "debug" module, in the top-level.

---
+++ cmake/rsp/debug.cmake
@@ -0,0 +1,40 @@
+# -------------------------------------------------------------------------------------------------------------- #
+# Debug utilities functions
+# -------------------------------------------------------------------------------------------------------------- #
+
+include_guard(GLOBAL)
+
+# Debug
+message(VERBOSE "rsp/debug module included")
+
+if (NOT COMMAND "dump")
+
+    #! dump : Outputs given variables' name and value
+    #
+    # @param ... Variables to output
+    #
+    function(dump)
+        foreach (var ${ARGN})
+            message("${var} = ${${var}}")
+        endforeach ()
+
+        # Output as warning so that the developer is able to see call stack!
+        message(WARNING "   ${CMAKE_CURRENT_FUNCTION}() called from ${CMAKE_CURRENT_LIST_FILE}")
+    endfunction()
+endif ()
+
+if (NOT COMMAND "dd")
+
+    #! dump and die: Outputs given variables' name and value and stops build
+    #
+    # @param ... Variables to output
+    #
+    function(dd)
+        foreach (var ${ARGN})
+            message("${var} = ${${var}}")
+        endforeach ()
+
+        # Output as fatal error to ensure that build stops.
+        message(FATAL_ERROR "   ${CMAKE_CURRENT_FUNCTION}() called from ${CMAKE_CURRENT_LIST_FILE}")
+    endfunction()
+endif ()
