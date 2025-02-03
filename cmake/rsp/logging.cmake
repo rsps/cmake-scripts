1725b3c08c7ae93d92bcb7515d5a0e6dca44f4f4
alin <alin@rspsystems.com>
2025-02-03 15:59:49 +0100

    Add remaining logging functions from PSR
    Need to make a small macro to help forward calls to log(), or the overhead of boilerplate script would be too much...

--- cmake/rsp/logging.cmake
+++ cmake/rsp/logging.cmake
@@ -28,6 +28,217 @@ include("rsp/logging/utils")
 # Log functions
 # -------------------------------------------------------------------------------------------------------------- #
 
+if (NOT COMMAND "emergency")
+
+    #! emergency : Log an "emergency" level message - System is unusable.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(emergency message)
+        forward_to_log(EMERGENCY_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "alert")
+
+    #! alert : Log an "alert" level message - Action must be taken immediately.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(alert message)
+        forward_to_log(ALERT_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "critical")
+
+    #! critical : Log an "critical" level message - Critical conditions.
+    # E.g. Application component unavailable, unexpected exception.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(critical message)
+        forward_to_log(CRITICAL_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "error")
+
+    #! error : Log an "error" level message.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(error message)
+        forward_to_log(ERROR_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "warning")
+
+    #! warning : Log an "warning" level message - Exceptional occurrences that are not errors.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(warning message)
+        forward_to_log(WARNING_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "notice")
+
+    #! notice : Log an "notice" level message - Normal but significant events.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(notice message)
+        forward_to_log(NOTICE_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "info")
+
+    #! info : Log an "info" level message - Normal but significant events.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    #                                           NOTICE, when not specified.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(info message)
+        forward_to_log(INFO_LEVEL ${message})
+    endfunction()
+endif ()
+
+if (NOT COMMAND "debug")
+
+    #! debug : Log an "debug" level message - Normal but significant events.
+    #
+    # @see log()
+    #
+    # @param <string|variable|list> message     The message to output. If a variable is given, its value will be used.
+    #                                           If a list is detected given, then each item in the list will be output,
+    #                                           using the LIST_SEPARATOR.
+    # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
+    #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
+    #                                           NOTICE, when not specified.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
+    # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
+    #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
+    #
+    # @return
+    #       [OUTPUT]                            The resulting output variable, if OUTPUT was specified.
+    #
+    function(debug message)
+        forward_to_log(DEBUG_LEVEL ${message})
+    endfunction()
+endif ()
+
 if (NOT COMMAND "log")
 
     #! log : Log a message with an arbitrary level
@@ -40,9 +251,10 @@ if (NOT COMMAND "log")
     #                                           using the LIST_SEPARATOR.
     # @param [<mode>]                           Option - Cmake's message type. Defaults to the `mode` that is
     #                                           associated with the specified `level`, defined in RSP_LOG_LEVELS_CMAKE.
-    #                                           NOTICE, when not specified.
-    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead of
-    #                                           being printed to stdout or stderr.
+    # @param [CONTEXT <variable>...]            Optional - Evt. variables to output in a "context" associated with the
+    #                                           log message.
+    # @param [OUTPUT <variable>]                Optional - If specified, message is assigned to output variable instead
+    #                                           of being printed to stdout or stderr.
     # @param [LIST_SEPARATOR <string>]          Optional - Separator to used, if a list variable is given as message.
     #                                           Defaults to RSP_DEFAULT_LIST_SEPARATOR.
     #
@@ -71,7 +283,7 @@ if (NOT COMMAND "log")
         resolve_cmake_message_mode()
 
         # ---------------------------------------------------------------------------------------------- #
-        
+
         # Format message label, acc. to the log level
         format_log_level_label("${log_level}" label)
 
@@ -102,7 +314,7 @@ if (NOT COMMAND "log")
         # ---------------------------------------------------------------------------------------------- #
 
         # Context
-        if (DEFINED INPUT_CONTEXT)
+        if (DEFINED INPUT_CONTEXT AND NOT INPUT_CONTEXT STREQUAL "")
             format_log_context("${log_level}" "${INPUT_CONTEXT}" formatted_context)
             string(APPEND buffer "${formatted_context}")
         endif ()
@@ -121,18 +333,63 @@ if (NOT COMMAND "log")
 
         # ---------------------------------------------------------------------------------------------- #
 
-        # Assign to output variable, if requested and stop any further processing.
-        if (DEFINED INPUT_OUTPUT)
-            set("${INPUT_OUTPUT}" "${label}${formatted_output}")
-            return(PROPAGATE "${INPUT_OUTPUT}")
-        endif ()
-
-        # ---------------------------------------------------------------------------------------------- #
-
         # Finally, output the log message
         output(${formatted_output}
             "${msg_mode}"
+            OUTPUT ${INPUT_OUTPUT}
             LABEL "${label}"
         )
+
+        # ---------------------------------------------------------------------------------------------- #
+
+        # Assign to output variable, if requested and stop any further processing.
+        if (DEFINED INPUT_OUTPUT)
+            return(PROPAGATE "${INPUT_OUTPUT}")
+        endif ()
     endfunction()
+endif ()
+
+# -------------------------------------------------------------------------------------------------------------- #
+# Internals
+# -------------------------------------------------------------------------------------------------------------- #
+
+if (NOT COMMAND forward_to_log)
+
+    #! forward_to_log : Macro that forwards current function call to log()
+    #
+    # WARNING: Macro is intended to be used internally within various logging
+    # level functions, w.g. critical(), error(), warning()...etc
+    # It is responsible for forwarding arguments to the log() function and
+    # return evt. assigned OUTPUT.
+    #
+    # @see log()
+    #
+    # @internal
+    #
+    macro(forward_to_log log_level message)
+        set(options "${RSP_CMAKE_MESSAGE_MODES}")
+        set(oneValueArgs OUTPUT LIST_SEPARATOR)
+        set(multiValueArgs CONTEXT)
+
+        cmake_parse_arguments(PARSE_ARGV 1 INPUT "${options}" "${oneValueArgs}" "${multiValueArgs}")
+
+        # ---------------------------------------------------------------------------------------------- #
+
+        extract_value(log_level ${log_level})
+        resolve_cmake_message_mode()
+
+        # ---------------------------------------------------------------------------------------------- #
+
+        log("${log_level}" ${message} "${msg_mode}"
+            OUTPUT ${INPUT_OUTPUT}
+            CONTEXT ${INPUT_CONTEXT}
+            LIST_SEPARATOR ${INPUT_LIST_SEPARATOR}
+        )
+
+        # ---------------------------------------------------------------------------------------------- #
+
+        if (DEFINED INPUT_OUTPUT)
+            return(PROPAGATE "${INPUT_OUTPUT}")
+        endif ()
+    endmacro()
 endif ()
