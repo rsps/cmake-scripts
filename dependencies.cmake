9e906e81d460ca5c5a615d25a2f8c6f87cb7143b
alin <alin@rspsystems.com>
2025-02-08 14:42:41 +0100

    Change messages to VERBOSE mode
    No need to pollute the console with too many messages.

--- dependencies.cmake
+++ dependencies.cmake
@@ -5,13 +5,13 @@
 include_guard()
 
 function(install_dependencies)
-    message(STATUS "Installing Dependencies for ${PROJECT_NAME}")
+    message(VERBOSE "Installing Dependencies for ${PROJECT_NAME}")
 
     # Avoid building tests for dependencies...
     set(BUILD_TESTING off)
 
     # Add dependencies here...
-    message(STATUS "    N/A")
+    message(VERBOSE "    N/A")
 
 endfunction()
 safeguard_properties(CALLBACK "install_dependencies" PROPERTIES BUILD_TESTING)
